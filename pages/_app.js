4fade8ef2f0cd603949e6f7176ca5daf73531429
Katch <katchvidal@gmail.com>
2021-10-15 09:25:38 -0500

    simple portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+// importing bootswatch
+import 'bootswatch/dist/cosmo/bootstrap.min.css'
+import 'animate.css/animate.min.css'
+import '../global.css'
 
-function MyApp({ Component, pageProps }) {
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
---
+++ profile.js
@@ -0,0 +1,103 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 80,
+  },
+  {
+    skill: "Python",
+    percentage: 80,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 90,
+  },
+  {
+    skill: "Django",
+    percentage: 80,
+  },
+  {
+    skill: "React",
+    percentage: 70,
+  },
+  {
+    skill: "Next JS",
+    percentage: 70,
+  },
+  {
+    skill: "GraphQL",
+    percentage: 80,
+  },
+  {
+    skill: "Apollo Server / Client ",
+    percentage: 80,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Soporte Tecnico Empresa Industrial Tabasco",
+    description:
+      "Soporte Tecnico para Gestionar, Guardar Informacion, Revision de Equipos de Empresa",
+    from: 2018,
+    to: 2018,
+  },
+  {
+    title: "Developer Paginas Web",
+    description:
+      "Dedicado al Desarrollo Web de Paginas Estaticas para Comercios",
+    from: 2017,
+    to: 2020,
+  },
+  {
+    title: "Devoloper Full Stack",
+    description:
+      "Desarrollando Backend para Aplicaciones de Tercero y el Esqueleto del Frontend para Pasarlos al Profesional de UI ",
+    from: 2020,
+    to: "Since",
+  },
+];
+
+export const projects = [
+  {
+    name: "Login Authorizaction",
+    description:
+      "Login de Aplicacion de Pagina Web Con JSON WEB TOKEN recibiendolo y procesandolo en el backend con Apollo Server todo hecho desde cero",
+    image: "foto1.jpg",
+  },
+  {
+    name: "Servicios AWS",
+    description:
+      "Conectando Aplicacion de NEXT JS y Strappi con AWS AMAZON WEB SERVICES S3 BUCKET",
+    image: "foto2.jpg",
+  },
+  {
+    name: "Django",
+    description:
+      "Aplicacion de Django - CRUD - Login - Proteccion de Rutas con Mixin y Login Required ",
+    image: "foto3.jpg",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
